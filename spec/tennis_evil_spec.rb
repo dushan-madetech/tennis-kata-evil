a0cad7eb151c4f5a94f7ea562f8f838bc94c7f63
Yusuf <yusuf@madetech.com>
2019-02-11 16:19:46 +0000

    Third test passed

--- spec/tennis_evil_spec.rb
+++ spec/tennis_evil_spec.rb
@@ -1,13 +1,22 @@
 class Tennis
+  def initialize
+    @player_one_points = 0
+  end
+
   def score(player = :p2)
     if(player == :p1)
-      player_one_points = 0
-      while(player_one_points < 15)
-        player_one_points += 1
+      while(@player_one_points < 15)
+        @player_one_points += 1
       end
-      return player_one_points.to_s + "-0"
+      return @player_one_points.to_s + "-0"
     end
     return '0-0'
+  end
+
+  def increase_score
+    if @player_one_points < 30 
+      @player_one_points = 30 
+    end
   end
 end
 
@@ -21,6 +30,7 @@ describe Tennis do
   end
 
   it "returns a score of 30-0 when player One scores another point" do
+    subject.increase_score
     expect(subject.score(:p1)).to eq('30-0')
   end
 end
