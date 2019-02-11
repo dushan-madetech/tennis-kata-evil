ae3832f5dbb86c3d94b1e132c54fd1367cb90a85
MadeTech Dushan <dushan@madetech.com>
2019-02-11 15:56:39 +0000

    Pass second test

--- spec/tennis_evil_spec.rb
+++ spec/tennis_evil_spec.rb
@@ -1,5 +1,12 @@
 class Tennis
-  def score
+  def score(player = :p2)
+    if(player == :p1)
+      player_one_points = 0
+      while(player_one_points < 15)
+        player_one_points += 1
+      end
+      return player_one_points.to_s + "-0"
+    end
     return '0-0'
   end
 end
