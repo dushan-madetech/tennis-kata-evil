c9edb290f3533845af9ab1398d4ffbf7977084a4
Yusuf <yusuf@madetech.com>
2019-02-11 15:50:21 +0000

    Add the second test case

--- spec/tennis_evil_spec.rb
+++ spec/tennis_evil_spec.rb
@@ -6,6 +6,10 @@ end
 
 describe Tennis do
   it "can start a game" do
-    expect(subject.score).to eq("0-0")
+    expect(subject.score).to eq('0-0')
+  end
+
+  it "returns a score of 15-0 when player One scores a point" do
+    expect(subject.score(:p1)).to eq('15-0')
   end
 end
